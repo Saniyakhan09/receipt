4d2958f66fd0c8a3d733fef45da99261b5e2d126
unknown <saniyakhan7019@gmail.com>
2025-10-10 20:11:20 +0530

    changed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,6 +14,7 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser());
+
 app.use('/receipt', receiptRoutes);
 app.use('/user', authRoutes);
 app.use(express.static(path.join(__dirname, '../Public/dist')));
